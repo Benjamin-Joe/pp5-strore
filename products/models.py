7aca7ba66e67051ef32dd9313edc45056d169131
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-12-18 15:43:03 +0000

    Add product and category models to models.py, add them to admin.py

---
+++ products/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """
+    Model for creating categories
+    """
+    name = models.CharField(max_length=200)
+
+
+def __str__(self):
+    return self.name
+
+
+class Product(models.Model):
+    """
+    Model for adding products to website
+    """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
