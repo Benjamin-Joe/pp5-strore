b79fa5bc1adbe76a8da6871cf135abb070f3f50b
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-12-19 00:32:27 +0000

    Fix indentation error on caterogy model. Add product images to media folder

--- products/models.py
+++ products/models.py
@@ -8,8 +8,8 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
 
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
