1eebc7a1e892f5171066157d6a8145111a15884f
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-12-20 19:16:43 +0000

    Add order by price and rating low to high and high to low to navbar

--- products/views.py
+++ products/views.py
@@ -11,8 +11,24 @@ def Products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -27,10 +43,13 @@ def Products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(price__icontains=query)
             products = products.filter(queries)
 
+    new_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'new_sorting': new_sorting
     }
     return render(request, 'products/products.html', context)
 
