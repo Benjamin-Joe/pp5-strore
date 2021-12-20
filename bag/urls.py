96408f9096218ead3e4df16e20c754b5070f512b
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-12-20 21:44:19 +0000

    Added add to bag, code from boutique ado

--- bag/urls.py
+++ bag/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bag/', views.bag, name='bag')
+    path('bag/', views.bag, name='bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+"""
+Code below came mostly from Code Institute
+"""
 
 
 def bag(request):
@@ -7,3 +11,19 @@ def bag(request):
     all of the users items and purchases
     """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """Adding items to shopping basket"""
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
