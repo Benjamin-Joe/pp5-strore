94cc461cfe6af7ee67304fdb906c1ae14379444c
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-12-22 14:39:33 +0000

    Add change quantity functionality to bag

--- bag/urls.py
+++ bag/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('bag/', views.bag, name='bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
-
+    path('edit/<item_id>/', views.adjust_bag, name='adjust_bag'),
 ]
