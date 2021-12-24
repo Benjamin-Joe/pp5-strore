3dc6255c59dd6a9b86bbdbbb36d620aa269a59d1
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-12-24 21:50:13 +0000

    Deployment commit

--- estore/settings.py
+++ estore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)xx@#8)s2iu!2u)=t-96gk_avoieaxjaiug#4o4)l)59w_r-h1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['klarart-1.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -116,13 +116,17 @@ WSGI_APPLICATION = 'estore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
